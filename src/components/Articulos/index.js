f01e2000aadc4981393b9e937406359658a962f5
gustavocano23 <gustavo00inmare@gmail.com>
2021-12-05 23:30:56 -0600

    Corrgiendo producto en useEffect

--- src/components/Articulos/index.js
+++ src/components/Articulos/index.js
@@ -7,7 +7,7 @@ export const Articulos = () => {
     
     
     let {articulos} = useContext(UseProduct)
-    const [productos, setProductos] = useState(articulos)
+    const [productos, setProductos] = useState([])
     const [update, setUpdate] = useState(false)
     const [showButton, setShowButton] = useState(true)
     
@@ -16,11 +16,11 @@ export const Articulos = () => {
     let buscarProducto = useRef(null)
 
     //Hook useEffect para renderizar todos los productos una vez que haya buscado
-    useEffect(()=>{setShowButton(true)}, [update])
+    useEffect(()=>{setProductos([]);setShowButton(true)}, [update])
 
     const handleSearch = () =>{
         let productBySearch = buscarProducto.current.value
-        let findProduct =  productos.find((prod) => prod.nombre === productBySearch)
+        let findProduct =  articulos.find((prod) => prod.nombre === productBySearch)
         if (!productBySearch || !findProduct){setUpdate(!update); return}
         setShowButton(false)
         setProductos([findProduct])
@@ -33,8 +33,8 @@ export const Articulos = () => {
                     <TxtSearch ref={buscarProducto} type="text" placeholder="Nombre del producto a buscar"  />
                     <BtnSearch onClick={handleSearch}>Buscar producto</BtnSearch>
                 </>
-            ): <BtnSearch style={{marginTop:100}} onClick={()=>{setUpdate(!update)}}>Mostrar Todos los productos</BtnSearch>}
-            <Container>{productos.map(prod => <Articulo key={prod.id} prod={prod}/>)}</Container>
+            ): <BtnSearch onClick={()=>{setUpdate(!update)}}>Mostrar Todos los productos</BtnSearch>}
+            <Container>{(productos.length) ? productos.map(prod => <Articulo key={prod.id} prod={prod}/>):articulos.map(prod => <Articulo key={prod.id} prod={prod}/>) }</Container>
         </>
     )
 }
